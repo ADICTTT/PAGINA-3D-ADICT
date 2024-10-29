73c9b51da759c4953927d4daf4e5881425a1ca92
ADICTTT <choquecallatadriana@gmail.com>
2024-10-28 21:32:57 -0400

    avance de la pagina principal

--- adict/src/slices/Hero/index.tsx
+++ adict/src/slices/Hero/index.tsx
@@ -1,5 +1,7 @@
 import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import { Bounded } from "@/components/Bounded";
 
 /**
  * Props for `Hero`.
@@ -11,12 +13,26 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  */
 const Hero = ({ slice }: HeroProps): JSX.Element => {
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      Placeholder component for hero (variation: {slice.variation}) Slices
-    </section>
+      <div className="grid">
+        <div className="grid h-screen place-items-center">
+          <PrismicRichText field={slice.primary.heading} />
+          <PrismicRichText field={slice.primary.subheading} />
+          <PrismicRichText field={slice.primary.body} />
+          <PrismicNextLink field={slice.primary.button_link} />
+          {slice.primary.button_text}
+        </div>
+
+        <div className="text-side relative z-[80] grid h-screen items-center gap-4 md:grid-cols-2">
+          <PrismicNextImage field={slice.primary.cans_image} />
+          <PrismicRichText field={slice.primary.second_heading} />
+          <PrismicRichText field={slice.primary.second_body} />
+        </div>
+      </div>
+      </Bounded>
   );
 };
 
